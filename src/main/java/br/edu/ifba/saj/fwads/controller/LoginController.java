80f76a3c63db17d14d2ccc097ef0398787429655
gabriel <gabriel.lealsantos@hotmail.com>
2025-01-16 13:47:08 -0300

    Alerts personalizados

--- src/main/java/br/edu/ifba/saj/fwads/controller/LoginController.java
+++ src/main/java/br/edu/ifba/saj/fwads/controller/LoginController.java
@@ -1,5 +1,6 @@
 package br.edu.ifba.saj.fwads.controller;
 
+import br.edu.ifba.saj.fwads.controller.util.Alerts;
 import br.edu.ifba.saj.fwads.model.entities.Cliente;
 import br.edu.ifba.saj.fwads.model.exception.LoginInvalidoException;
 import br.edu.ifba.saj.fwads.model.service.ValidaCliente;
@@ -7,30 +8,29 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
 public class LoginController {
 
-    @FXML // fx:id="txSenha"
-    private PasswordField txSenha; // Value injected by FXMLLoader
-
-    @FXML // fx:id="txUsuario"
-    private TextField txUsuario; // Value injected by FXMLLoader
+    @FXML
+    private PasswordField txSenha;
 
     @FXML
-    void entrar(ActionEvent event) {
+    private TextField txUsuario;
+
+    @FXML
+    private Button btnEntra;
+
+    @FXML
+    public void entrar(ActionEvent event) {
         try {
             // Valida o login com o serviço ValidaCliente
             Cliente cliente = new ValidaCliente().validaLogin(txUsuario.getText(), txSenha.getText());
 
-            // Carrega a próxima tela e configura o MasterController
-
-
-
         } catch (LoginInvalidoException e) {
-            // Exibe alerta de erro no login
-            new Alert(AlertType.ERROR, e.getMessage()).showAndWait();
+            Alerts.showAlert("Login invalido", "Login ou senha inválidos. Por favor, verifique as informações e tente novamente.","Erro de Login", AlertType.ERROR);
         }
     }
 
